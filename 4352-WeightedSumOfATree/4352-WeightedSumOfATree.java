95961fe10e23204ea848869a73f779ac78108086
Madhav <madhavkumar7639@gmail.com>
2026-08-11 21:00:39 +0530

    Create 4352-WeightedSumOfATree

---
+++ 4352-WeightedSumOfATree/4352-WeightedSumOfATree.java
@@ -0,0 +1,37 @@
+// Last updated: 11/08/2026, 21:01:08
+class Solution {
+    public long weightedSum(int[] parent, int[] nums) {
+       int n= parent.length;
+
+        int[][] malviretho=new int[][]{parent,nums};
+        List<Integer>[] children=new ArrayList[n];
+
+        for(int i=0;i<n;i++){
+            children[i]=new ArrayList<>();
+        }
+        for(int i=1;i<n;i++)
+            children[parent[i]].add(i);
+
+        int[] depth=new int[n];
+
+        Queue<Integer> queue=new LinkedList<>();
+        queue.offer(0);
+        depth[0]=1;
+        int height=1;
+
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            for(int child:children[node]){
+                depth[child]=depth[node]+1;
+                height=Math.max(height,depth[child]);
+                queue.offer(child);
+            }
+        }
+        long ans=0;
+        for(int i=0;i<n;i++){
+            long weight=(long)nums[i]*(height-depth[i]+1);
+            ans+=weight;
+        }
+        return ans;
+    }
+}
