6ac0ee0337800ce08d2a41171ad79633efe2213e
Madhav <madhavkumar7639@gmail.com>
2026-08-11 21:05:03 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.java
@@ -0,0 +1,14 @@
+// Last updated: 11/08/2026, 21:05:32
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1)
+        return false;
+        while(n%3==0)
+        n=n/3;
+        if(n==1)
+        return true;
+        else
+        return false;
+        
+    }
+}
