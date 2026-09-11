5a41bf109fe5d9ecb8a9a784338445d9037d2c92
Madhav <madhavkumar7639@gmail.com>
2026-09-11 09:41:45 +0530

    Create 1823-FindTheWinnerOfTheCircularGame

---
+++ 1823-FindTheWinnerOfTheCircularGame/1823-FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,15 @@
+// Last updated: 11/09/2026, 09:42:17
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> q=new LinkedList<>();
+         for(int i=1;i<=n;i++)
+         q.add(i);
+        
+        while(q.size()!=1){
+            for(int i=0;i<k-1;i++)
+            q.add(q.poll());
+            q.poll();
+        }
+        return q.peek();
+    }
+}
