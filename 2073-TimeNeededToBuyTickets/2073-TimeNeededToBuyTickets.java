097f9f35a3440490a565f236ebcd5144673e279c
Madhav <madhavkumar7639@gmail.com>
2026-09-11 09:41:22 +0530

    Create 2073-TimeNeededToBuyTickets

---
+++ 2073-TimeNeededToBuyTickets/2073-TimeNeededToBuyTickets.java
@@ -0,0 +1,16 @@
+// Last updated: 11/09/2026, 09:41:54
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time += Math.min(tickets[i], tickets[k]);
+            } else {
+                time += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return time;
+    }
+}
