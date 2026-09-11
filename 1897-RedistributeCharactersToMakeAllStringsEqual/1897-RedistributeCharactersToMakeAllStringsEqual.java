53b49a8dda61f74788e93e5048231a3a9acbc41e
Madhav <madhavkumar7639@gmail.com>
2026-09-11 09:41:34 +0530

    Create 1897-RedistributeCharactersToMakeAllStringsEqual

---
+++ 1897-RedistributeCharactersToMakeAllStringsEqual/1897-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,22 @@
+// Last updated: 11/09/2026, 09:42:06
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int[] freq = new int[26];
+
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+        }
+
+        int n = words.length;
+
+        for (int count : freq) {
+            if (count % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
